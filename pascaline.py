0b6d52c32e33e1a12566989c5c86abe3c1e38c08
achraf_nait_belkacem <achrafoonb@gmail.com>
2026-01-13 10:35:31 +0100

    fonction add-sub-mult-div

---
+++ pascaline.py
@@ -0,0 +1,38 @@
+def add(numbre1, numbre2):
+    result = numbre1 * numbre2
+    print (result)
+
+def sub(numbre1, numbre2):
+    result = numbre1 * numbre2
+    print (result)
+
+def mult(numbre1, numbre2):
+    result = numbre1 * numbre2
+    print (result)
+
+def div(numbre1, numbre2):
+    if numbre2 == 0:
+        print("division by 0 impossible")
+    else:
+        result = numbre1 * numbre2
+        print (result)
+    
+while True:
+    
+    numbre1 = int(input("entre a numbre :"))
+
+    operation = str(input("what operation you wanna use :"))
+
+    numbre2 = int(input("entre a numbre :"))
+    
+    while True:
+        if operation == "+":
+            add(numbre1,numbre2)
+        elif operation == "-":
+            sub(numbre1,numbre2)
+        elif operation == "*":
+            mult(numbre1,numbre2)
+        elif operation == "/":
+            div(numbre1,numbre2)
+        else:
+            print("opps wrong choice!")
